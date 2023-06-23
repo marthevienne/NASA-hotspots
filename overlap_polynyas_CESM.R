221cca5ae586e3b691a803569d740247dd0a164f
marthevienne <123016211+marthevienne@users.noreply.github.com>
2023-06-23 13:58:33 -0400

    Update overlap_polynyas_CESM.R

--- overlap_polynyas_CESM.R
+++ overlap_polynyas_CESM.R
@@ -1,7 +1,7 @@
 ##
-## Script name: 
+## Script name: overlap_polynyas_CESM.R
 ##
-## Purpose of script:
+## Purpose of script: Compute mean number of cells identified as polynyas by month that fall in polynya polygons
 ##
 ## Author: Marthe Vienne
 ## Modified by:
@@ -13,7 +13,8 @@
 ##
 ## ---------------------------
 ##
-## Notes:
+## Notes: To have an idea of how many cells fall in individual polynyas polygons, the identification of polynya cells is required.
+##        Not done here.
 ##   
 ##
 ## ---------------------------
@@ -24,13 +25,17 @@ rm(list=ls())
 ## Library
 library(raster)
 library(ncdf4)
+library(dplyr)
+library(stringr)
+library(terra)
+library(tidyr)
+library(ggplot2)
 ## ---------------------------
-
-source("~/Desktop/WHOI/Codes/functions_raster/netcdf_to_raster.R")
 
 #==================================================================
 # MONTHLY NCAR polynyas 
 #==================================================================
+source("~/Desktop/WHOI/Codes/functions_raster/netcdf_to_raster.R")
 
 file <- "~/Dropbox/data/polynya_contours_NCAR_2023/Low res run/JRA_4p2z_run/g.e22.GOMIPECOIAF_JRA-1p4-2018.TL319_g17.4p2z.001branch.hi_0.4mthresh.polynya_sh.195801-202112"
 
@@ -79,6 +84,7 @@ df_polynyas <- df_polynyas %>%
   mutate(new_id = paste0(ID, "-", num),
          date = paste0(month, ".", year))
 
+id_pols <- unique(df_polynyas$ID)
 dates <- unique(names(r)) #___ordered array
 dates <- dates[dates %in% unique(df_polynyas$date)] #___keep only dates present in df_polynyas
 
@@ -89,10 +95,10 @@ colnames(summary) = c("layer", "n_cells_NCAR", "n_cells_overlap", "year", "month
 
 i = 1
 r_new <- list()
-bbox <- extent(c(min(df_polynyas$lon), max(df_polynyas$lon), -70, -60))
+bbox <- extent(c(min(df_polynyas$lon), max(df_polynyas$lon), -70, -60)) #___crop the polynya grid to the area where polynyas polygons are found
 
 for (d in dates) {
-  print(paste(floor(i/length(dates) * 100), "%"))
+  print(i)
   
   df_polynyas_lyr <- df_polynyas %>%
     filter(date == d)
@@ -111,19 +117,24 @@ for (d in dates) {
   r2 <- r[[lyr_raster]]
   r3 <- crop(r2, bbox)
   
-  SpP_ras <- rasterize(SPDF, r3, getCover=TRUE)
-  SpP_ras[SpP_ras == 0] <- NA
-
-  r4 <- mask(r3, SpP_ras)
-  # r4 <- mask(r3, SPDF)
+  if (nrow(df_polynyas_lyr) > 0) {
+    SpP_ras <- rasterize(SPDF, r3, getCover = T)
+    SpP_ras[SpP_ras == 0] <- NA
+    r4 <- mask(r3, SpP_ras) #___OPTION 1: all intersection between cells and polygons
+    # r4 <- mask(r3, SPDF) #___OPTION 2: only cells with there centroids falling in polygons are accounted for
+    
+    summary$n_cells_overlap[i] = length(r4[r4 >= 1])
+  } else {
+    summary$n_cells_overlap[i] = NA
+  }
   
   r_new[[i]] <- r4
   
   summary$layer[i] = d
   summary$n_cells_NCAR[i] = length(r3[r3 >= 1])
-  summary$n_cells_overlap[i] = length(r4[r4 >= 1])
   
   i = i + 1
+  
 }
 
 summary <- summary %>%
@@ -137,16 +148,22 @@ terra::writeRaster(r_new, "~/Desktop/WHOI/Data/polynyas_contours/NCAR/JRA_4p2z_r
 ## Save summary
 write.csv(summary, "~/Desktop/WHOI/Data/polynyas_contours/NCAR/JRA_4p2z_run/summary_overlap_NCAR_OBS.csv", row.names = F)
 
+#==================================================================
+# BIMODAL BARPLOT BY MONTH => # CELLS
+#==================================================================
+rm(list=ls())
 
-test <- summary %>% 
+summary <- read.csv("~/Desktop/WHOI/Data/polynyas_contours/NCAR/JRA_4p2z_run/summary_overlap_NCAR_OBS.csv")
+
+summary_mean <- summary %>% 
   gather(n_cells_NCAR, n_cells_overlap, 2:3) %>%
   rename(n_cells = n_cells_overlap, type = n_cells_NCAR) %>%
   group_by(month, type) %>%
   summarise(n_cells = mean(n_cells))
 
-test$month <- factor(test$month, levels = month.name)
+summary_mean$month <- factor(summary_mean$month, levels = month.name)
 
-n_cells_barplot <- ggplot(test, aes(x = month, fill = factor(type))) + 
+n_cells_barplot <- ggplot(summary_mean, aes(x = month, fill = factor(type))) + 
   geom_bar(aes(y = n_cells), stat = "identity", position = "dodge") +
   scale_fill_viridis_d(option = "H", "Cells") +
   xlab("Month")+
@@ -165,11 +182,23 @@ n_cells_barplot <- ggplot(test, aes(x = month, fill = factor(type))) +
 
 ggsave("~/Dropbox/data/outputs_Marthe_2023/polynya contours/cells_overlap_barplot.png", height = 30, width = 30, units = c("cm"), dpi = 300)
 
-## ---- Overlap on one layer
+
+#==================================================================
+# OVERLAP ON ONE LAYER
+#==================================================================
+rm(list=ls())
+
+d = "August.2004" #___sample date
+source("~/Desktop/WHOI/Codes/useful_functions/df_to_SpatialPolygonsDataFrame.R")
+
+r <- raster::brick("~/Desktop/WHOI/Data/polynyas_contours/NCAR/JRA_4p2z_run/NCAR_monthly_polynya_2004_2019.tiff")
 r_new <- raster::brick("~/Desktop/WHOI/Data/polynyas_contours/NCAR/JRA_4p2z_run/NCAR_polynya_cells_crop_polygons_2004_2019.tiff")
+df_polynyas <- read.csv("~/Desktop/WHOI/Data/polynyas_contours/OBS/all_contours_per_month/OBS_multiple_polynyas_contours_df.csv")
 
 df_polynyas_lyr <- df_polynyas %>%
-  filter(date == "August.2004")
+  mutate(new_id = paste0(ID, "-", num),
+         date = paste0(month, ".", year)) %>%
+  filter(date == d)
 
 keep_poly <- df_polynyas_lyr %>%
   group_by(new_id) %>%
@@ -183,9 +212,9 @@ SPDF <- df_to_SpatialPolygonsDataFrame(df_polynyas_lyr, split_polygons = "new_id
 
 e <- extent(SPDF)
 
-pdf("~/Dropbox/data/outputs_Marthe_2023/polynya contours/map_overlap_NCAR_OBS.pdf", width = 7, height = 4)
-print(plot(e, lwd = .0, col = "white", main = "August 2004"))
-print(plot(r$August.2004, add = T, col = "red", 
+pdf("~/Dropbox/data/outputs_Marthe_2023/polynya contours/example_overlap_NCAR_OBS.pdf", width = 7, height = 4)
+print(plot(e, lwd = .0, col = "white", main = d))
+print(plot(r$August.2004, add = T, col = "blue", 
            useRaster = FALSE, legend = F))
 print(plot(r_new$August.2004, add = T, col = "green",
            useRaster = FALSE, legend = F))
