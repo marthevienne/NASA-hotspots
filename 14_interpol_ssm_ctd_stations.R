40af635481cdc9c86adb25a75ed6f3a15d8654e1
marthevienne <123016211+marthevienne@users.noreply.github.com>
2023-07-11 14:09:42 +0200

    Update 14_interpol_ssm_ctd_stations.R
    - divide filtering steps
    - add summary filtering process

--- 14_interpol_ssm_ctd_stations.R
+++ 14_interpol_ssm_ctd_stations.R
@@ -40,7 +40,10 @@ seals$REF <- gsub("_", "-", seals$REF)
 
 seals %>%
   filter(!REF %in% stations$REF) %>%
+  filter(visit_polynya == "yes") %>%
   select(c(REF, has_dives, visit_polynya))
+
+length(unique(stations$REF))
 
 # Step0: number of CTD profiles per seal
 step0 <- stations %>%
@@ -52,8 +55,13 @@ seals <- seals %>% left_join(step0, by = "REF")
 # Step1: number of CTD profiles under northern boundary or latitude = NA
 northern_boundary <- -60
 
+stations <- stations %>%
+  filter(is.na(lat) | lat < northern_boundary)
+
+file_loc = "~/Desktop/WHOI/Data/output_data/ctd_stations_table_north_bound"
+saveRDS(stations, file_loc)
+
 step1 <- stations %>%
-  filter(is.na(lat) | lat < northern_boundary) %>%
   group_by(REF) %>%
   summarise(n_ctd_step1 = n()) 
 
@@ -70,12 +78,12 @@ write.table(seals, file_output_seals, sep = ",", row.names = F, col.names = T)
 rm(list=ls())
 
 filter_on_ssm_accuracy = TRUE
-threshold = 20 #km
+threshold = 10 #km
 north_boundary = -60
 # TRUE => ctd stations closest in time from poorly accurate predicted locations are filtered with a upper threshold on standard error. 
 # FALSE => all ctd locations are kept
 
-stations <- readRDS("~/Desktop/WHOI/Data/output_data/ctd_stations_table")
+stations <- readRDS("~/Desktop/WHOI/Data/output_data/ctd_stations_table_north_bound")
 SSM = readRDS("~/Desktop/WHOI/Data/output_data/predictedTracks_ssm")
 seals <- read.csv("~/Desktop/WHOI/Data/output_data/seals.csv")
 
@@ -108,25 +116,72 @@ for (p in 1:length(seals)) {
   df_interpol <- rbind(df_interpol, sa)
 }
 
+nrow(df_interpol) / nrow(stations) * 100 #___ % kept stations
+
 file_interp_loc = "~/Desktop/WHOI/Data/output_data/ctd_stations_table_interp"
 saveRDS(df_interpol, file_interp_loc)
 
-# Step2: number of CTD profiles after reinterpolation (interpLat < northern boundary)
-northern_boundary <- -60
-
+# Step2: number of CTD profiles after estimation of location (interpLat != NA)
 seals <- read.csv("~/Desktop/WHOI/Data/output_data/seals.csv")
 
+df_interpol <- df_interpol %>%
+  filter(!is.na(interpLat))
+
 step2 <- df_interpol %>%
-  filter(is.na(interpLat) | interpLat < northern_boundary) %>%
   group_by(REF) %>%
   summarise(n_ctd_step2 = n()) 
 
 seals <- seals %>% left_join(step2, by = "REF")
 
+# Step 3: number of CTD stations south of north boundary after estimation of location
+north_boundary <- -60
+
+df_interpol <- df_interpol %>%
+  filter(interpLat < north_boundary)
+
+step3 <- df_interpol %>%
+  group_by(REF) %>%
+  summarise(n_ctd_step3 = n())
+
+seals <- seals %>% left_join(step3, by = "REF")
+
+file_interp_loc = "~/Desktop/WHOI/Data/output_data/ctd_stations_table_north_bound_interp"
+saveRDS(df_interpol, file_interp_loc)
+
 ## Save updated seals table ======================================
 file_output_seals =  "~/Desktop/WHOI/Data/output_data/seals.csv"
 write.table(seals, file_output_seals, sep = ",", row.names = F, col.names = T)
 ##================================================================
+
+#==================================================================
+# 3) SUMMARY FILTERING PROCESS
+#==================================================================
+rm(list=ls())
+
+seals <- read.csv("~/Desktop/WHOI/Data/output_data/seals.csv")
+
+summary_ctd <- seals %>%
+  select(contains("ctd"))
+
+tot <- colSums(summary_ctd, na.rm = T)
+tot / tot[1] * 100
+
+description <- paste0("unit: % \n",
+                      "Step 0: number of CTD profiles per seal \n",
+                      "Step 1: number of CTD profiles under northern boundary or latitude = NA \n",
+                      "Step 2: number of CTD profiles after estimation of location (interpLat != NA) \n",
+                      "Step 3: number of CTD stations south of north boundary after estimation of location \n")
+#-----Save output
+sink("~/Desktop/WHOI/Data/summary_outputs/filtering_ctd_stations.txt")
+cat(paste("Last modified :", Sys.Date()))
+cat("\n\n")
+cat(description)
+cat("\n")
+print(tot / tot[1] * 100)
+cat("\n")
+sink()
+#----------------
+
 
 ## End script
 rm(list=ls())
