7a4362dfd92e986a4986b8923922b726b0e6e084
marthevienne <123016211+marthevienne@users.noreply.github.com>
2023-08-13 16:03:44 +0200

    Create df_to_SpatialPolygons.R

---
+++ useful_functions/df_to_SpatialPolygons.R
@@ -0,0 +1,16 @@
+df_to_SpatialPolygons <- function(df, split_polygons) {
+  var = colnames(df)
+  rm_var <- var[!var %in% c("lon", "lat")]
+  
+  i = which(var == split_polygons)
+  list_pol = split(df, f = df[ ,i])
+  for (v in rm_var) {
+    list_pol <- lapply(list_pol, function(x) { x[v] <- NULL; x })
+  }
+  
+  p_pol <- lapply(list_pol, Polygon)
+  ps_pol <- lapply(seq_along(p_pol), function(k) Polygons(list(p_pol[[k]]), ID = names(p_pol)[k]))
+  sps_pol <- SpatialPolygons(ps_pol)
+  # print(plot(sps_pol))
+  return(sps_pol)
+}
