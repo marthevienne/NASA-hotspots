e50b03591994ac8aa6fbd4b1a4b84bdef1917634
marthevienne <123016211+marthevienne@users.noreply.github.com>
2023-08-22 11:49:00 +0200

    Directory NASA CAP analysis

---
+++ NASA_CAP_Sara/palettes/palette_polynya.R
@@ -0,0 +1,65 @@
+outside = "#E5E5E5"
+CapeDarnley = "#ED7A5F"
+Shackleton = "#80CAAC"
+Davis = "#A489D8"
+BarrierBay = "#F4BC44"
+
+pal = c(
+  "0" = outside,
+  "1" = "#ED7A5F",
+  "2" = "#21908CFF",
+  "3" = "#A489D8",
+  "4" = "#F4BC44",
+  "5" = "#0C0927FF",
+  "6" = "#365C8DFF",
+  "7" = "#80CAAC",
+  "8" = "#FEBA80FF",
+  "9" = "#481B6DFF",
+  "10" = "#FDDC9EFF",
+  "11" = "#FDE725FF",
+  "12" = "#982D80FF",
+  "13" =  "#FB9E07FF",
+  "14" = "#D84842FF",
+  "15" = "#F57D15FF",
+  "16" = "#348FA7FF",
+  "17" = "#5DC863FF",
+  "18" = "#DA7800",
+  "19" = "#9E9800",
+  "20" = "#23ADE4",
+  "21" = "#EE7496"
+)
+
+
+# dives = readRDS("summary_dives_id_pol")
+# str(dives)
+# 
+# unique(dives$in_polynya)
+
+pal_CESM_met = c("0" = outside, 
+                 "8" = "#FEBA80FF",
+                 "3" = "#A489D8",
+                 "2" = "#21908CFF",
+                 "5" = "#0C0927FF",
+                 "6" = "#365C8DFF",
+                 "7" = "#80CAAC",
+                 "16" = "#348FA7FF",
+                 "4" = "#F4BC44",
+                 "15" = "#F57D15FF",
+                 "9" = "#481B6DFF",
+                 "14" = "#D84842FF",
+                 "11" = "#FDE725FF",
+                 "12" = "#982D80FF"
+                 )
+
+
+# "#440154FF" "#481B6DFF" "#46337EFF" "#3F4889FF" "#365C8DFF" "#2E6E8EFF" "#277F8EFF" "#21908CFF" "#1FA187FF"
+# "#2DB27DFF" "#4AC16DFF" "#71CF57FF" "#9FDA3AFF" "#CFE11CFF" "#FDE725FF"
+# 
+# "#000004FF" "#0C0927FF" "#231151FF" "#410F75FF" "#5F187FFF" "#7B2382FF" "#982D80FF" "#B63679FF" "#D3436EFF"
+# "#EB5760FF" "#F8765CFF" "#FD9969FF" "#FEBA80FF" "#FDDC9EFF" "#FCFDBFFF"
+# 
+# "#000004FF" "#0D082AFF" "#280B54FF" "#480B6AFF" "#65156EFF" "#82206CFF" "#9F2A63FF" "#BB3754FF" "#D44842FF"
+# [10] "#E8602DFF" "#F57D15FF" "#FB9E07FF" "#FAC127FF" "#F3E55CFF" "#FCFFA4FF"
+# 
+# "#0B0405FF" "#1E111FFF" "#2E1E3CFF" "#3B2D5AFF" "#413D7BFF" "#3E5095FF" "#37659EFF" "#357BA2FF" "#348FA7FF"
+# [10] "#36A3ABFF" "#40B7ADFF" "#58CBADFF" "#8AD9B1FF" "#B9E6C7FF" "#DEF5E5FF"
---
+++ NASA_CAP_Sara/palettes/palette_season.R
@@ -0,0 +1,22 @@
+pal_season = c("Autumn" = "#A489D8",
+               "Summer" = "#F57D15FF",
+               "Winter" = "#21908CFF",
+               "Spring" = "#F4BC44")
+  
+
+
+
+  c("January" = "#982D80FF",
+              "February" = "#FEBA80FF",
+              "March" = "#A489D8",
+              "April" = "#21908CFF",
+              "May" = "#0C0927FF",
+              "June" = "#365C8DFF",
+              "July" = "#80CAAC",
+              # "16" = "#348FA7FF",
+              "August" = "#F4BC44",
+              "September" = "#F57D15FF",
+              "October" = "#481B6DFF",
+              "November" = "#D84842FF",
+              "December" = "#FDE725FF"
+)
