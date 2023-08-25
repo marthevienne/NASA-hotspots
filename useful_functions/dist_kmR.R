838ff4d3097a058bbfdec2589f6a9b010d1fbf30
marthevienne <123016211+marthevienne@users.noreply.github.com>
2023-08-25 18:02:14 +0200

    correct function

--- useful_functions/dist_kmR.R
+++ useful_functions/dist_kmR.R
@@ -6,10 +6,26 @@ dist_km <- function(lat1, lat2, lon1, lon2) {
   return(res)
 }
 
-dist_km <- function(dlat, dlon) {
-  require(okara)
-  mean_lat <- mean(dlat)
-  deg <- sqrt(abs(dlon)^2 + abs(dlat)^2)
-  res <- d2km(deg, base.latitude = mean_lat)
-  return(res)
-}
+# dist_km <- function(lat, dlon) {
+#   require(okara)
+#   mean_lat <- mean(dlat)
+#   deg <- sqrt(abs(dlon)^2 + abs(dlat)^2)
+#   res <- d2km(deg, base.latitude = mean_lat)
+#   return(res)
+# }
+# 
+# dlat = c(-68.70417, -66.62083)
+# dlon = c(65, 65)
+# 
+# lat1 = -68.70417
+# lat2 = lat1 + 0.0416667
+# lon1 = 65
+# lon2 = lon1
+# 
+# dist_km(lat1, lat2, lon1, lon2)
+# 
+# 
+# lat1 = -68.70417
+# lat2 = lat1
+# lon1 = 65
+# lon2 = lon1 + 0.0416667
