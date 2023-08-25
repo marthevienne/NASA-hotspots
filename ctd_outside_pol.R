8646b8b9ad350f708bfcfa5ce487de8773058d35
marthevienne <123016211+marthevienne@users.noreply.github.com>
2023-08-25 17:58:26 +0200

    Create ctd_outside_pol.R

---
+++ ctd_outside_pol.R
@@ -0,0 +1,65 @@
+dives_outside <- readRDS("behavioural_data/dive_metrics_bottime_speed_interp_hunttime_bathy_zone_mode_pol_inpol_ctd_9") %>%
+  filter(pol == 0 & !is.na(id_ctd)) %>%
+  mutate(month = month(DE_DATE)) %>%
+  filter(month >= 6 & month <= 8)
+
+hist(dives_outside$MAX_DEP)
+
+id_ctd_dives <- dives_outside %>%
+  pull(id_ctd) %>%
+  unique() %>%
+  na.omit()
+
+## CTD stations
+stations <- readRDS("ctd_data/ctd_stations_table_north_bound_interp") %>%
+  filter(id_ctd %in% id_ctd_dives)
+
+lonLat <- stations %>%
+  dplyr::select(c(id_ctd, interpLon, interpLat))
+
+## CTD profiles in polynyas
+ctd <- readRDS("ctd_data/ctd_profiles_table") %>%
+  filter(id_ctd %in% id_ctd_dives) %>%
+  left_join(lonLat, by = "id_ctd")
+
+## Dataframe for computing
+df <- ctd %>%
+  dplyr::select(c(id = id_ctd, depth, SP = psal, t = temp, lon = interpLon, lat = interpLat))
+
+## Calculate MLD
+df_mld <- compute_mld(df, eos = "gsw")
+summary(df_mld$MLD)
+
+## Add MLD to stations table
+stations_MLD <- stations %>%
+  left_join(df_mld, by = c("id_ctd" = "id"))
+
+## Scatter plot of MLD versus day of the year
+ggplot(stations_MLD, aes(x = yday(time), fill = id_ctd, col = REF)) + 
+  geom_point(aes(y = -MLD), size = .2) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  scale_color_viridis_d()
+
+saveRDS(stations_MLD, "ctd_data/ctd_stations_table_north_bound_interp_MLD")
+
+## MLD == NA ?
+stations_na <- readRDS("ctd_data/ctd_stations_table_north_bound_interp_MLD") %>%
+  filter(is.na(MLD))
+
+stations_na %>% count()
+summary(stations_na$max_depth)
+hist(stations_na$max_depth, breaks = 30)
+
+#----> CTD not deep enough (med = 454.0 m)
+
+## MLD
+stations <- readRDS("ctd_data/ctd_stations_table_north_bound_interp_MLD") %>%
+  filter(!is.na(MLD))
+
+stations %>% count()
+summary(stations$max_depth)
+hist(stations$max_depth, breaks = 30)
+
+ggplot(stations) +
+  geom_point(aes(x = max_depth, y = MLD))
